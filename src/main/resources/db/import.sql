9a63f2650a8256d74b8069d246879d5f3c8cd14b
Thomas Sødring <tsodring@gmail.com>
2018-06-26 11:35:20 +0200

    Create a grouse@kdrs.no user for demo purposes.

--- src/main/resources/db/import.sql
+++ src/main/resources/db/import.sql
@@ -7,7 +7,7 @@ INSERT INTO user (username, password, account_non_expired,
 
 INSERT INTO user (username, password, account_non_expired,
                   credentials_non_expired, account_non_locked, enabled) VALUES (
-  'user@kdrs.no',
+  'grouse@kdrs.no',
   '$2a$10$Gk3cjYgN0GJd04VkG5R8/OxZ.QVuR.ZH.fN.1z9Jqy8wdgiWoDzqq',
    true, true, true, true);
 
@@ -15,7 +15,7 @@ INSERT INTO user (username, password, account_non_expired,
 INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 INSERT INTO authority (authority_name) VALUES ('ROLE_USER');
 
-INSERT INTO user_authority (username, authority) VALUES ('user@kdrs.no', 'ROLE_USER');
+INSERT INTO user_authority (username, authority) VALUES ('grouse@kdrs.no', 'ROLE_USER');
 INSERT INTO user_authority (username, authority) VALUES ('admin@kdrs.no', 'ROLE_USER');
 INSERT INTO user_authority (username, authority) VALUES ('admin@kdrs.no', 'ROLE_ADMIN');
 
