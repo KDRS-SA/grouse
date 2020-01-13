b9995c32d03d973ebe5e89ec44680082023dc236
Unknown <spidikh@icloud.com>
2020-01-13 15:12:15 +0100

    Initial experimentation with MDL
    Tried some things, cleaned up a bit, development on login page starts here

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatSliderModule} from '@angular/material';
+import {MatCardModule, MatSliderModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
