5bdb512ff633b8f20a63f660690985617161b1fb
Unknown <spidikh@icloud.com>
2020-01-17 18:28:15 +0100

    Made a communicationtest function
    A simple test that was devised to learn how to comunicate with the server, this will be expanded apon to create the rest of the loggin functions

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,14 @@
 import {Component, Inject} from '@angular/core';
 import {Animations} from './app.animations';
 import {FormControl, Validators} from '@angular/forms';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
+// Lar klienten konfigurere HttpClienten, som brukes for å komunisere med serveren
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) {}
+}
 
 @Component({
   selector: 'app-root',
@@ -15,6 +22,8 @@ export class AppComponent {
   public title = 'Grouse';
   public login: boolean;
 
+  private http: HttpClient;
+
   email = new FormControl('',
     [Validators.required, Validators.email]);
 
@@ -23,11 +32,18 @@ export class AppComponent {
     this.email.hasError('email') ? 'Ikke en gyldig E-post Adresse' : '';
   }
 
-  constructor() {
+  constructor(http: HttpClient) {
     this.login = true;
+    this.http = http;
   }
 
   public changeMode() {
     this.login = !this.login;
+  }
+
+  public httpGetTest() {
+    this.http.get<string>('http://localhost:9294/grouse/').subscribe(result => {
+      console.log(result);
+    }, error => console.error(error));
   }
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatCardModule, MatSliderModule} from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -23,7 +23,8 @@ import {ReactiveFormsModule} from '@angular/forms';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
