8d3208354c0aaaa3a6739e12fe8e4463a3edaf7b
Thomas Sødring <tsodring@gmail.com>
2020-02-07 13:59:48 +0100

    Introduce IntrnalException
    Can be thrown when something unexpected goes wrong server side

---
+++ src/main/java/no/kdrs/grouse/utils/exception/InternalException.java
@@ -0,0 +1,9 @@
+package no.kdrs.grouse.utils.exception;
+
+public class InternalException
+        extends RuntimeException {
+
+    public InternalException(String message) {
+        super(message);
+    }
+}
