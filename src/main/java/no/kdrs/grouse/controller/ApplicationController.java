265f64314a84e7881bb9423b7b7283d99beffaa1
Thomas Sødring <tsodring@gmail.com>
2020-03-27 14:28:37 +0100

    Remove unused logger and create comment. Comment not relevant.

--- src/main/java/no/kdrs/grouse/controller/ApplicationController.java
+++ src/main/java/no/kdrs/grouse/controller/ApplicationController.java
@@ -4,8 +4,6 @@ package no.kdrs.grouse.controller;
 import no.kdrs.grouse.assemblers.APIAssembler;
 import no.kdrs.grouse.model.APIDetail;
 import no.kdrs.grouse.model.links.LinksAPIDetail;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
-/**
- * Created by tsodring on 24/06/18.
- */
 @RestController
 @RequestMapping
 public class ApplicationController {
 
-    private static final Logger logger =
-            LoggerFactory.getLogger(ApplicationController.class);
-
-    APIAssembler apiAssembler;
+     APIAssembler apiAssembler;
 
     public ApplicationController(APIAssembler apiAssembler) {
         this.apiAssembler = apiAssembler;
