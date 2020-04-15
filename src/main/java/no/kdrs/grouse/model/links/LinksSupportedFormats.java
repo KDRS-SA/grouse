6530b0bffd3d4dac061bc4d3399c3d7dd1624645
Thomas Sødring <tsodring@gmail.com>
2020-04-15 15:00:28 +0200

    Tidy up warnings

--- src/main/java/no/kdrs/grouse/model/links/LinksSupportedFormats.java
+++ src/main/java/no/kdrs/grouse/model/links/LinksSupportedFormats.java
@@ -1,20 +1,17 @@
 package no.kdrs.grouse.model.links;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
-import no.kdrs.grouse.model.Project;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import no.kdrs.grouse.model.SupportedFormats;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
-import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
+@EqualsAndHashCode(callSuper = false)
 @Data
 @Relation(collectionRelation = "supportedFormats",
         itemRelation = "supportedFormat")
